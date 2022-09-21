1e7e8a260b3283307fa887be26300fb29604799a
Walker <walker@praiseweb.com.br>
2022-09-21 16:08:50 -0300

    Use lovelaceAcademy only deps

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
               src = ./.;
             } + /node_modules;
           package =
-            with purs-nix.ps-pkgs;
+            with purs-nix.ps-pkgs-ns.lovelaceAcademy;
             {
               version = "6.0.0";
               dependencies =
